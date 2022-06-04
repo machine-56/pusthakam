0c0fadce9adb7f4892fdde0b3958711429216428
machine-56 <itwasanoption@gmail.com>
2022-06-04 23:38:44 +0530

    commit

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mode:Ap123TTb@library-data.2ubiv3w.mongodb.net/Library',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Library', {
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
